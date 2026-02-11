aef07f543dcbfad700c1192d5d9585d87baae4c5
denisse11u <denisse11crespo@gmail.com>
2026-02-11 15:08:14 -0500

    search

--- app/lib/modules/home/page/home_page.dart
+++ app/lib/modules/home/page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:app/modules/authenticate/login/page/login_page.dart';
-import 'package:app/modules/home/widget/create_data.dart';
 import 'package:app/modules/wordspace/wigdet/create_wordspace.dart';
 import 'package:app/shared/storage/user_storage.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final storage = Userstorage();
+  final searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +40,41 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.add_circle_outline_sharp),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: searchController,
+          style: const TextStyle(fontSize: 15),
+          decoration: InputDecoration(
+            hintText: 'Buscar texto',
+            hintStyle: TextStyle(color: Colors.grey[400]),
+            prefixIcon: const Icon(
+              Icons.search_rounded,
+              color: Color(0xFF667eea),
+            ),
+            suffixIcon: searchController.text.isNotEmpty
+                ? IconButton(
+                    icon: const Icon(Icons.clear_rounded, size: 20),
+                    onPressed: () {
+                      setState(() {
+                        searchController.clear();
+                      });
+                    },
+                  )
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide.none,
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 16,
+            ),
+          ),
+        ),
       ),
 
       // body: Padding(
